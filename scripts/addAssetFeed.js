13f79615bb8736257b01ac1844d2b8ada0c85430
kenchangh <guanhao3797@gmail.com>
2020-11-03 19:02:25 +0000

    update addAssetFeed

--- scripts/addAssetFeed.js
+++ scripts/addAssetFeed.js
@@ -3,36 +3,45 @@ require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 var json = require("../addresses.json");
 var dataProviderJSON = require("../build/contracts/DataProvider.json");
+const ADDRESSES = require("../constants/externalAddresses.json");
 
 async function addAssetFeed(asset, feed) {
   const dataProviderAddress = json["contracts"]["dataProvider"];
   const adminAddress = json["accounts"]["admin"];
 
-  const contract = new web3.eth.Contract(dataProviderJSON.abi, dataProviderAddress);
-  await contract.methods
-    .addChainlinkFeed(asset, feed)
-    .send({ 
+  const contract = new web3.eth.Contract(
+    dataProviderJSON.abi,
+    dataProviderAddress
+  );
+  await contract.methods.addChainlinkFeed(asset, feed).send(
+    {
       from: adminAddress,
       gasPrice: "10000000000", // 10 Gwei
       gasLimit: "80000", // 80k gas limit
-      value: "0"
-    }, function (error, transactionHash) {
+      value: "0",
+    },
+    function (error, transactionHash) {
       console.log("Txhash: ", transactionHash);
-    });
+    }
+  );
 }
 
 async function getFeed(asset) {
   const dataProviderAddress = json["contracts"]["dataProvider"];
   const adminAddress = json["accounts"]["admin"];
 
-  const contract = new web3.eth.Contract(dataProviderJSON.abi, dataProviderAddress);
-  await contract.methods
-    .getChainlinkFeed(asset)
-    .call({ 
+  const contract = new web3.eth.Contract(
+    dataProviderJSON.abi,
+    dataProviderAddress
+  );
+  await contract.methods.getChainlinkFeed(asset).call(
+    {
       from: adminAddress,
-    }, function (error, result) {
+    },
+    function (error, result) {
       console.log(result);
-    });
+    }
+  );
 }
 
 module.exports = async function (done) {
@@ -42,14 +51,11 @@ module.exports = async function (done) {
   );
   web3.setProvider(provider);
 
-  const dai = "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa";
-  const daiFeed = "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541"
-  const link = "0xa36085f69e2889c224210f603d836748e7dc0088"
-  const linkFeed = "0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38";
+  const usdc = ADDRESSES.kovan.assets.usdc;
+  const usdcFeed = ADDRESSES.kovan.feeds["eth/usd"];
 
-  await addAssetFeed(link, linkFeed);
-  await addAssetFeed(dai, daiFeed);
-  // await getFeed(dai);
+  await addAssetFeed(usdc, usdcFeed);
+  await getFeed(usdc);
   // await getFeed(link);
 
   done();

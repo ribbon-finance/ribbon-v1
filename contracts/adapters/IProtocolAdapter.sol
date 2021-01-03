342515eca7ccef70269a4be7938841b92ded5a2f
Ken Chan <guanhao3797@gmail.com>
2021-01-03 12:33:08 +0800

    finished commenting adapters (#30)

--- contracts/adapters/IProtocolAdapter.sol
+++ contracts/adapters/IProtocolAdapter.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.6.0;
 enum OptionType {Invalid, Put, Call}
 
 interface IProtocolAdapter {
+    /**
+     * @notice Emitted when a new option contract is purchased
+     */
     event Purchased(
         address indexed caller,
         string indexed protocolName,
@@ -17,6 +20,9 @@ interface IProtocolAdapter {
         uint256 optionID
     );
 
+    /**
+     * @notice Emitted when an option contract is exercised
+     */
     event Exercised(
         address indexed caller,
         address indexed options,
@@ -25,10 +31,25 @@ interface IProtocolAdapter {
         uint256 exerciseProfit
     );
 
+    /**
+     * @notice Name of the adapter. E.g. "HEGIC", "OPYN_V1". Used as index key for adapter addresses
+     */
     function protocolName() external pure returns (string memory);
 
+    /**
+     * @notice Boolean flag to indicate whether to use option IDs or not.
+     * Fungible protocols normally use tokens to represent option contracts.
+     */
     function nonFungible() external pure returns (bool);
 
+    /**
+     * @notice Check if an options contract exist based on the passed parameters.
+     * @param underlying is the underlying asset of the options. E.g. For ETH $800 CALL, ETH is the underlying.
+     * @param strikeAsset is the asset used to denote the asset paid out when exercising the option. E.g. For ETH $800 CALL, USDC is the underlying.
+     * @param expiry is the expiry of the option contract. Users can only exercise after expiry in Europeans.
+     * @param strikePrice is the strike price of an optio contract. E.g. For ETH $800 CALL, 800*10**18 is the USDC.
+     * @param optionType is the type of option, can only be OptionType.Call or OptionType.Put
+     */
     function optionsExist(
         address underlying,
         address strikeAsset,
@@ -37,6 +58,14 @@ interface IProtocolAdapter {
         OptionType optionType
     ) external view returns (bool);
 
+    /**
+     * @notice Get the options contract's address based on the passed parameters
+     * @param underlying is the underlying asset of the options. E.g. For ETH $800 CALL, ETH is the underlying.
+     * @param strikeAsset is the asset used to denote the asset paid out when exercising the option. E.g. For ETH $800 CALL, USDC is the underlying.
+     * @param expiry is the expiry of the option contract. Users can only exercise after expiry in Europeans.
+     * @param strikePrice is the strike price of an optio contract. E.g. For ETH $800 CALL, 800*10**18 is the USDC.
+     * @param optionType is the type of option, can only be OptionType.Call or OptionType.Put
+     */
     function getOptionsAddress(
         address underlying,
         address strikeAsset,
@@ -45,6 +74,14 @@ interface IProtocolAdapter {
         OptionType optionType
     ) external view returns (address);
 
+    /**
+     * @notice Gets the premium to buy `purchaseAmount` of the option contract in ETH terms.
+     * @param underlying is the underlying asset of the options. E.g. For ETH $800 CALL, ETH is the underlying.
+     * @param strikeAsset is the asset used to denote the asset paid out when exercising the option. E.g. For ETH $800 CALL, USDC is the underlying.
+     * @param expiry is the expiry of the option contract. Users can only exercise after expiry in Europeans.
+     * @param strikePrice is the strike price of an optio contract. E.g. For ETH $800 CALL, 800*10**18 is the USDC.
+     * @param optionType is the type of option, can only be OptionType.Call or OptionType.Put
+     */
     function premium(
         address underlying,
         address strikeAsset,
@@ -54,12 +91,27 @@ interface IProtocolAdapter {
         uint256 purchaseAmount
     ) external view returns (uint256 cost);
 
+    /**
+     * @notice Amount of profit made from exercising an option contract (current price - strike price). 0 if exercising out-the-money.
+     * @param options is the address of the options contract
+     * @param optionID is the ID of the option position in non fungible protocols like Hegic.
+     * @param amount is the amount of tokens or options contract to exercise. Only relevant for fungle protocols like Opyn
+     */
     function exerciseProfit(
         address options,
         uint256 optionID,
         uint256 amount
     ) external view returns (uint256 profit);
 
+    /**
+     * @notice Purchases the options contract.
+     * @param underlying is the underlying asset of the options. E.g. For ETH $800 CALL, ETH is the underlying.
+     * @param strikeAsset is the asset used to denote the asset paid out when exercising the option. E.g. For ETH $800 CALL, USDC is the underlying.
+     * @param expiry is the expiry of the option contract. Users can only exercise after expiry in Europeans.
+     * @param strikePrice is the strike price of an optio contract. E.g. For ETH $800 CALL, 800*10**18 is the USDC.
+     * @param optionType is the type of option, can only be OptionType.Call or OptionType.Put
+     * @param amount is the purchase amount in Wad units (10**18)
+     */
     function purchase(
         address underlying,
         address strikeAsset,
@@ -69,6 +121,13 @@ interface IProtocolAdapter {
         uint256 amount
     ) external payable returns (uint256 optionID);
 
+    /**
+     * @notice Exercises the options contract.
+     * @param options is the address of the options contract
+     * @param optionID is the ID of the option position in non fungible protocols like Hegic.
+     * @param amount is the amount of tokens or options contract to exercise. Only relevant for fungle protocols like Opyn
+     * @param recipient is the account that receives the exercised profits. This is needed since the adapter holds all the positions and the msg.sender is an instrument contract.
+     */
     function exercise(
         address options,
         uint256 optionID,

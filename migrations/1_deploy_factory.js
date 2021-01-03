6b7d1670e1ca11d242e6092202f03c11ffdb7215
kenchangh <guanhao3797@gmail.com>
2021-01-03 18:59:01 +0800

    deploy faactory

--- migrations/1_deploy_factory.js
+++ migrations/1_deploy_factory.js
@@ -1,5 +1,3 @@
-const DataProvider = artifacts.require("DataProvider");
-const LiquidatorProxy = artifacts.require("LiquidatorProxy");
 const Factory = artifacts.require("DojimaFactory");
 const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
 const { encodeCall } = require("@openzeppelin/upgrades");
@@ -19,16 +17,6 @@ module.exports = async function (deployer, network, accounts) {
     wethAddress = ADDRESSES[network].assets.weth;
   }
 
-  await deployer.deploy(DataProvider, wethAddress);
-  await updateDeployedAddresses(network, "DataProvider", DataProvider.address);
-
-  await deployLiquidatorProxy(deployer, admin, owner);
-  await updateDeployedAddresses(
-    network,
-    "LiquidatorProxy",
-    AdminUpgradeabilityProxy.address
-  );
-
   await deployFactory(deployer, admin, owner);
   await updateDeployedAddresses(
     network,
@@ -37,37 +25,13 @@ module.exports = async function (deployer, network, accounts) {
   );
 };
 
-async function deployLiquidatorProxy(deployer, admin, owner) {
-  await deployer.deploy(LiquidatorProxy);
-
-  const liquidatorProxy = LiquidatorProxy.address;
-
-  const initBytes = encodeCall(
-    "initialize",
-    ["address", "uint256"],
-    [owner, ether("1.05").toString()]
-  );
-  await deployer.deploy(
-    AdminUpgradeabilityProxy,
-    liquidatorProxy,
-    admin,
-    initBytes,
-    {
-      from: admin,
-    }
-  );
-}
-
 async function deployFactory(deployer, admin, owner) {
   await deployer.deploy(Factory);
-
-  const dataProvider = DataProvider.address;
-  const liquidatorProxy = AdminUpgradeabilityProxy.address;
 
   const initBytes = encodeCall(
     "initialize",
     ["address", "address", "address", "address"],
-    [owner, dataProvider, admin, liquidatorProxy]
+    [owner, admin]
   );
   await deployer.deploy(
     AdminUpgradeabilityProxy,

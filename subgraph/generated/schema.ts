168dca713dcb73d6228c077d8f525c3b661604ab
kenchangh <guanhao3797@gmail.com>
2021-01-30 02:05:00 +0800

    working build w purchase

--- subgraph/generated/schema.ts
+++ subgraph/generated/schema.ts
@@ -42,13 +42,87 @@ export class InstrumentPosition extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get cost(): BigInt {
-    let value = this.get("cost");
-    return value.toBigInt();
+  get positionID(): i32 {
+    let value = this.get("positionID");
+    return value.toI32();
   }
 
-  set cost(value: BigInt) {
-    this.set("cost", Value.fromBigInt(value));
+  set positionID(value: i32) {
+    this.set("positionID", Value.fromI32(value));
+  }
+
+  get account(): Bytes | null {
+    let value = this.get("account");
+    if (value === null || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set account(value: Bytes | null) {
+    if (value === null) {
+      this.unset("account");
+    } else {
+      this.set("account", Value.fromBytes(value as Bytes));
+    }
+  }
+
+  get cost(): BigInt | null {
+    let value = this.get("cost");
+    if (value === null || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set cost(value: BigInt | null) {
+    if (value === null) {
+      this.unset("cost");
+    } else {
+      this.set("cost", Value.fromBigInt(value as BigInt));
+    }
+  }
+}
+
+export class OptionPurchase extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save OptionPurchase entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save OptionPurchase entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("OptionPurchase", id.toString(), this);
+  }
+
+  static load(id: string): OptionPurchase | null {
+    return store.get("OptionPurchase", id) as OptionPurchase | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get instrumentPosition(): string {
+    let value = this.get("instrumentPosition");
+    return value.toString();
+  }
+
+  set instrumentPosition(value: string) {
+    this.set("instrumentPosition", Value.fromString(value));
   }
 
   get account(): Bytes {
@@ -58,5 +132,50 @@ export class InstrumentPosition extends Entity {
 
   set account(value: Bytes) {
     this.set("account", Value.fromBytes(value));
+  }
+
+  get underlying(): Bytes {
+    let value = this.get("underlying");
+    return value.toBytes();
+  }
+
+  set underlying(value: Bytes) {
+    this.set("underlying", Value.fromBytes(value));
+  }
+
+  get optionType(): i32 {
+    let value = this.get("optionType");
+    return value.toI32();
+  }
+
+  set optionType(value: i32) {
+    this.set("optionType", Value.fromI32(value));
+  }
+
+  get amount(): BigInt {
+    let value = this.get("amount");
+    return value.toBigInt();
+  }
+
+  set amount(value: BigInt) {
+    this.set("amount", Value.fromBigInt(value));
+  }
+
+  get premium(): BigInt {
+    let value = this.get("premium");
+    return value.toBigInt();
+  }
+
+  set premium(value: BigInt) {
+    this.set("premium", Value.fromBigInt(value));
+  }
+
+  get optionID(): i32 {
+    let value = this.get("optionID");
+    return value.toI32();
+  }
+
+  set optionID(value: i32) {
+    this.set("optionID", Value.fromI32(value));
   }
 }
--- subgraph/src/instrument.ts
+++ subgraph/src/instrument.ts
@@ -1,6 +1,34 @@
-// import { PositionCreated } from "../generated/templates/Instrument/Instrument";
-// import { InstrumentPosition } from "../generated/templates/Instrument/Instrument.ts";
+import {
+  PositionCreated,
+  Purchased,
+} from "../generated/templates/Instrument/Instrument";
+import { InstrumentPosition, OptionPurchase } from "../generated/schema";
 
-// export function handlePositionCreated(event: PositionCreated):void {
+export function handlePositionCreated(event: PositionCreated): void {
+  let position = new InstrumentPosition(event.transaction.hash.toHex());
+  position.positionID = event.params.positionID.toI32();
+  position.account = event.params.account;
+  position.save();
+}
 
-// }
+export function handleOptionPurchased(event: Purchased): void {
+  let txhash = event.transaction.hash.toHex();
+  let purchaseID = txhash + "-" + event.logIndex.toString();
+  let purchase = new OptionPurchase(purchaseID);
+  purchase.instrumentPosition = txhash;
+
+  let instrumentPosition = InstrumentPosition.load(txhash);
+  if (instrumentPosition === null) {
+    instrumentPosition = new InstrumentPosition(txhash);
+  }
+  instrumentPosition.cost = instrumentPosition.cost.plus(event.params.premium);
+
+  purchase.account = event.params.caller;
+  purchase.underlying = event.params.underlying;
+  purchase.optionType = event.params.optionType;
+  purchase.amount = event.params.amount;
+  purchase.premium = event.params.premium;
+  purchase.optionID = event.params.optionID.toI32();
+  purchase.save();
+  instrumentPosition.save();
+}

fce8a26d356f22a4adeeab541b3bb38c2eb076bd
Ken Chan <guanhao3797@gmail.com>
2021-03-05 00:38:34 +0800

    Deployment v0.1 (#68)
    * new deployment
    
    * New vol contract

--- migrations/3_hegic_adapter.js
+++ migrations/3_hegic_adapter.js
@@ -8,6 +8,7 @@ const ACCOUNTS = require("../constants/accounts.json");
 const HEGIC_ETH_OPTIONS = "0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2";
 const HEGIC_WBTC_OPTIONS = "0x3961245DB602eD7c03eECcda33eA3846bD8723BD";
 const WBTC_ADDRESS = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+const ETH_WBTC_PAIR = "0xBb2b8038a1640196FbE3e38816F3e67Cba72D940";
 const ETH_ADDRESS = constants.ZERO_ADDRESS;
 
 let deployer, owner;
@@ -28,6 +29,7 @@ async function deployHegicAdapter() {
     HEGIC_WBTC_OPTIONS,
     ETH_ADDRESS,
     WBTC_ADDRESS,
+    ETH_WBTC_PAIR,
     { from: owner }
   );
   await updateDeployedAddresses(

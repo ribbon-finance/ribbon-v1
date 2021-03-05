3f19fbf79e2bcb2851ee669aa907d707feadc66c
kenchangh <guanhao3797@gmail.com>
2021-03-05 16:14:44 +0800

    working deribit api

--- src/deribit/api.ts
+++ src/deribit/api.ts
@@ -2,10 +2,17 @@ import axios from "axios";
 import { BigNumber } from "ethers";
 import { ASSETS } from "../constants";
 
+require("dotenv").config();
+
+const clientID = process.env["DERIBIT_CLIENT_ID"];
+const clientSecret = process.env["DERIBIT_CLIENT_SECRET"];
+
 const tickerMap = {
   [ASSETS.WETH]: "ETH",
   [ASSETS.WBTC]: "BTC",
 };
+
+const DERIBIT_URL = "https://test.deribit.com/api/v2";
 
 export function getOptionName(
   underlying: string,
@@ -30,4 +37,70 @@ export function getOptionName(
   return `${ticker}-${day}${month}${year}-${strike}-${optionType}`;
 }
 
-console.log(getOptionName(ASSETS.WETH, 1614931200, "140000000000", false));
+export async function authenticate(): Promise<string> {
+  const data = {
+    client_id: clientID as string,
+    client_secret: clientSecret as string,
+    grant_type: "client_credentials",
+    scope: "trade:read_write",
+  };
+
+  const response = await deribitAPICall("auth", data, "public");
+  return response.result.access_token;
+}
+
+export async function getPositions(
+  accessToken: string,
+  currency: string,
+  instrumentType = "option"
+) {
+  const data = {
+    currency,
+    kind: instrumentType,
+  };
+  const response = await deribitAPICall(
+    "get_positions",
+    data,
+    "private",
+    accessToken
+  );
+  return response;
+}
+
+export async function deribitAPICall(
+  endpoint: string,
+  data: Record<string, string>,
+  endpointType = "public",
+  accessToken: string | null = null
+) {
+  if (!clientID || !clientSecret) {
+    throw new Error("DERIBIT_CLIENT_ID & DERIBIT_CLIENT_SECRET need to be set");
+  }
+
+  const urlParams = new URLSearchParams(data).toString();
+
+  try {
+    const res = await axios.get(
+      `${DERIBIT_URL}/${endpointType}/${endpoint}?${urlParams}`,
+      {
+        headers:
+          accessToken !== null
+            ? {
+                Authorization: `Bearer ${accessToken}`,
+              }
+            : {},
+      }
+    );
+    return res.data;
+  } catch (e) {
+    console.error(e.response.data);
+    throw e;
+  }
+}
+
+(async function () {
+  const accessToken = await authenticate();
+  console.log(await getPositions(accessToken, "ETH", "option"));
+})();
+
+// console.log(getOptionName(ASSETS.WETH, 1614931200, "140000000000", false));

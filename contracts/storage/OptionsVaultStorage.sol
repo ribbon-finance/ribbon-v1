931d390c551802d3cc8b0acf0b2e5a9635ead5af
Ken Chan <guanhao3797@gmail.com>
2021-03-18 19:27:16 +0800

    Ken/reentrancy guard upgradeable (#97)
    * Use upgradeable version of ReentrancyGuard
    
    * make gap private

--- contracts/storage/OptionsVaultStorage.sol
+++ contracts/storage/OptionsVaultStorage.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.7.2;
 
 import {
-    ReentrancyGuard
-} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+    ReentrancyGuardUpgradeable
+} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import {
     OwnableUpgradeable
 } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
@@ -15,13 +15,13 @@ import {IRibbonFactory} from "../interfaces/IRibbonFactory.sol";
 
 contract OptionsVaultStorageV0 {
     // Gap just in case we push storage in front of the OptionsVaultStorage contract
-    uint256[50] __gap;
+    uint256[50] private __gap;
 }
 
 contract OptionsVaultStorageV1 is
+    ReentrancyGuardUpgradeable,
     OwnableUpgradeable,
-    ERC20Upgradeable,
-    ReentrancyGuard
+    ERC20Upgradeable
 {
     // Asset for which we create a covered call for
     address public asset;
@@ -52,4 +52,10 @@ contract OptionsVaultStorageV1 is
     address public feeRecipient;
 }
 
-contract OptionsVaultStorage is OptionsVaultStorageV1 {}
+// We are following Compound's method of upgrading new contract implementations
+// When we need to add new storage variables, we create a new version of OptionsVaultStorage
+// e.g. OptionsVaultStorageV<versionNumber>, so finally it would look like
+// contract OptionsVaultStorage is OptionsVaultStorageV1, OptionsVaultStorageV2
+contract OptionsVaultStorage is OptionsVaultStorageV1 {
+
+}

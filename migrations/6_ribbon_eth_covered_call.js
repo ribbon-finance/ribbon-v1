0a3f49fdc0fef5232e1d747981308b04c6670599
Chan Jing Hong <cjinghong@users.noreply.github.com>
2021-04-15 19:11:11 +0800

    Deploy WBTC to kovan network (#118)
    * Add migration file for wbtc covered call
    
    * Add newly deployed wbtc theta with updated migration file
    
    * Optimize migration file to not deploy protocolAdapterLogic twice
    
    * Add protocolAdapter migration file
    
    * Update minimum supply
    
    * Update contract name
    
    * Add migration file for wbtc covered call
    
    * Add newly deployed wbtc theta with updated migration file
    
    * Optimize migration file to not deploy protocolAdapterLogic twice
    
    * Add protocolAdapter migration file
    
    * Update minimum supply
    
    * Update contract name

--- migrations/6_ribbon_eth_covered_call.js
+++ migrations/6_ribbon_eth_covered_call.js
@@ -2,7 +2,7 @@ const RibbonCoveredCall = artifacts.require("RibbonCoveredCall");
 const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
 const ProtocolAdapterLib = artifacts.require("ProtocolAdapter");
 const { encodeCall } = require("@openzeppelin/upgrades");
-const { ethers } = require("ethers");
+const { ethers, BigNumber } = require("ethers");
 const { parseEther } = ethers.utils;
 
 const {
@@ -16,48 +16,46 @@ module.exports = async function (deployer, network) {
   const networkLookup = network.replace("-fork", "");
   const { admin, owner } = ACCOUNTS[networkLookup];
 
-  // Deploying the ProtocolAdapter
-  // await deployer.deploy(ProtocolAdapterLib);
+  await ProtocolAdapterLib.deployed();
 
-  // await updateDeployedAddresses(
-  //   network,
-  //   "ProtocolAdapterLib",
-  //   ProtocolAdapterLib.address
-  // );
+  await deployer.link(ProtocolAdapterLib, RibbonCoveredCall);
 
-  // await deployer.link(ProtocolAdapterLib, RibbonCoveredCall);
-
-  // // Deploying the logic contract
-  // await deployer.deploy(
-  //   RibbonCoveredCall,
-  //   DEPLOYMENTS[networkLookup].RibbonFactory,
-  //   EXTERNAL_ADDRESSES[networkLookup].assets.weth,
-  //   EXTERNAL_ADDRESSES[networkLookup].assets.usdc,
-  //   EXTERNAL_ADDRESSES[networkLookup].airswapSwap,
-  //   { from: admin }
-  // );
-  // await updateDeployedAddresses(
-  //   network,
-  //   "RibbonETHCoveredCallLogic",
-  //   RibbonCoveredCall.address
-  // );
+  // Deploying the logic contract
+  await deployer.deploy(
+    RibbonCoveredCall,
+    EXTERNAL_ADDRESSES[networkLookup].assets.weth,
+    DEPLOYMENTS[networkLookup].RibbonFactory,
+    EXTERNAL_ADDRESSES[networkLookup].assets.weth,
+    EXTERNAL_ADDRESSES[networkLookup].assets.usdc,
+    EXTERNAL_ADDRESSES[networkLookup].airswapSwap,
+    18,
+    // WETH: 10**18, 10**10 0.0000001
+    // WBTC: 0.000001
+    BigNumber.from("10").pow(BigNumber.from("10")).toString(), // WBTC 10**3
+    { from: admin }
+  );
+  await updateDeployedAddresses(
+    network,
+    "RibbonETHCoveredCallLogic",
+    RibbonCoveredCall.address
+  );
 
   // Deploying the proxy contract
   const initBytes = encodeCall(
     "initialize",
-    ["address", "address", "address", "uint256"],
+    ["address", "address", "uint256", "string", "string"],
     [
-      EXTERNAL_ADDRESSES[networkLookup].assets.weth,
       owner,
       owner,
       parseEther("1000").toString(),
+      "Ribbon ETH Theta Vault",
+      "rETH-THETA",
     ]
   );
 
   await deployer.deploy(
     AdminUpgradeabilityProxy,
-    // RibbonCoveredCall.address,
-    "0xEd61372660aeb0776d5385df2C5f99A462de0245",
+    RibbonCoveredCall.address,
     admin,
     initBytes,
     {
---
+++ migrations/7_ribbon_wbtc_covered_call.js
@@ -0,0 +1,69 @@
+const RibbonCoveredCall = artifacts.require("RibbonCoveredCall");
+const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
+const ProtocolAdapterLib = artifacts.require("ProtocolAdapter");
+const { encodeCall } = require("@openzeppelin/upgrades");
+const { ethers } = require("ethers");
+const { parseUnits } = ethers.utils;
+
+const {
+  updateDeployedAddresses,
+} = require("../scripts/helpers/updateDeployedAddresses");
+const ACCOUNTS = require("../constants/accounts.json");
+const DEPLOYMENTS = require("../constants/deployments.json");
+const EXTERNAL_ADDRESSES = require("../constants/externalAddresses.json");
+
+module.exports = async function (deployer, network) {
+  const networkLookup = network.replace("-fork", "");
+  const { admin, owner } = ACCOUNTS[networkLookup];
+
+  await ProtocolAdapterLib.deployed();
+
+  await deployer.link(ProtocolAdapterLib, RibbonCoveredCall);
+
+  // // Deploying the logic contract
+  await deployer.deploy(
+    RibbonCoveredCall,
+    EXTERNAL_ADDRESSES[networkLookup].assets.wbtc,
+    DEPLOYMENTS[networkLookup].RibbonFactory,
+    EXTERNAL_ADDRESSES[networkLookup].assets.weth,
+    EXTERNAL_ADDRESSES[networkLookup].assets.usdc,
+    EXTERNAL_ADDRESSES[networkLookup].airswapSwap,
+    8,
+    ethers.BigNumber.from("10").pow("3").toString(),
+    { from: admin }
+  );
+  await updateDeployedAddresses(
+    network,
+    "RibbonWBTCCoveredCallLogic",
+    RibbonCoveredCall.address
+  );
+
+  // Deploying the proxy contract
+  const initBytes = encodeCall(
+    "initialize",
+    ["address", "address", "uint256", "string", "string"],
+    [
+      owner,
+      owner,
+      ethers.BigNumber.from("10").pow("11").toString(), // 1000 (3 leading zeros) + 8 leading zeros
+      "Ribbon BTC Theta Vault",
+      "rBTC-THETA",
+    ]
+  );
+
+  await deployer.deploy(
+    AdminUpgradeabilityProxy,
+    RibbonCoveredCall.address,
+    admin,
+    initBytes,
+    {
+      from: admin,
+    }
+  );
+
+  await updateDeployedAddresses(
+    network,
+    "RibbonWBTCCoveredCall",
+    AdminUpgradeabilityProxy.address
+  );
+};

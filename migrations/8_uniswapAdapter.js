96c7266157280fb040de1da091471cc467a68fbc
Jacob Greene <69164627+jgcogsystematictrading@users.noreply.github.com>
2021-04-20 17:07:20 -0700

    Add files via upload

--- migrations/8_uniswapAdapter.js
+++ migrations/8_uniswapAdapter.js
@@ -18,5 +18,5 @@ module.exports = async function (deployer, _network) {
     //used already deployed uniswap contract on kovan
     //minted wbtc and digg + added liquidity to uniswap
     //used already deployed weth on kovan
-    await deployer.deploy(UniswapAdapter, '0x7a250d5630b4cf539739df2c5dacb4c659f2488d', '0x9d80a1aa0b68c7622818085314c3132ac51d5867', '0xd0a1e359811322d97991e03f863a0c30c2cf029c', '0x939295a7fe6eaa9456425535333f5937630ff963', '0xfd9907223857f25838da5ff65510fd81661fb261');
+    await deployer.deploy(UniswapAdapter, '0x7a250d5630b4cf539739df2c5dacb4c659f2488d', EXTERNAL_ADDRESSES[networkLookup].assets.wbtc2, EXTERNAL_ADDRESSES[networkLookup].assets.weth, EXTERNAL_ADDRESSES[networkLookup].assets.uniLp, EXTERNAL_ADDRESSES[networkLookup].assets.digg);
 }

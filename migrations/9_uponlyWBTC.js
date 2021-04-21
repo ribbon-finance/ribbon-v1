8ade30fc654b5431e8a3c40615905a92c05f8b92
Jacob Greene <69164627+jgcogsystematictrading@users.noreply.github.com>
2021-04-21 04:43:42 -0700

    Add files via upload

--- migrations/9_uponlyWBTC.js
+++ migrations/9_uponlyWBTC.js
@@ -36,6 +36,7 @@ module.exports = async function (deployer, _network) {
   await deployer.deploy(
     WBTCOptions,
     EXTERNAL_ADDRESSES[networkLookup].feeds.btc/usd,
+    //sushiswap kovan
     "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506",
     EXTERNAL_ADDRESSES[networkLookup].assets.wbtc2,
     EXTERNAL_ADDRESSES[networkLookup].hegicStakingWBTC
@@ -49,13 +50,10 @@ module.exports = async function (deployer, _network) {
   await deployer.link(AmmAdapterLib, StakedPut);
   await deployer.deploy(
     StakedPut,
-    //factory
-    "0x5A53a1C73a8e7e91291817B2D04B4d8468DE172d",
-    //uniswap adapter
-    "0xaed19b6922c162e8167f57684e16db49ea3acae3",
+    DEPLOYMENTS[networkLookup].RibbonFactory,
+    DEPLOYMENTS[networkLookup].UniswapAdapter,
     EXTERNAL_ADDRESSES[networkLookup].assets.wbtc2,
-    //wbtc options
-    "0x755AD189d2F1Be069214FaFC2c5c73a6e3d3cCB9",
+    EXTERNAL_ADDRESSES[networkLookup].hegicWBTCOptions,
     EXTERNAL_ADDRESSES[networkLookup].assets.usdc,
     EXTERNAL_ADDRESSES[networkLookup].feeds.btc/usd,
     { from: "" }

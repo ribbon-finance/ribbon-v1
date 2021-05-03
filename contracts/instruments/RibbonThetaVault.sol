6d114aff780d318f078e6bc86b3486d65d486c27
kenchangh <guanhao3797@gmail.com>
2021-05-03 21:20:43 +0100

    Emit events

--- contracts/instruments/RibbonThetaVault.sol
+++ contracts/instruments/RibbonThetaVault.sol
@@ -71,7 +71,7 @@ contract RibbonThetaVault is DSMath, OptionsVaultStorage {
 
     event CapSet(uint256 oldCap, uint256 newCap, address manager);
 
-    event ScheduleWithdraw(address account, uint256 withdrawID, uint256 amount);
+    event ScheduleWithdraw(address account, uint256 withdrawID, uint256 shares);
 
     event ScheduledWithdrawCompleted(
         address account,
@@ -301,6 +301,8 @@ contract RibbonThetaVault is DSMath, OptionsVaultStorage {
         queuedWithdrawShares = queuedWithdrawShares.add(shares);
 
         scheduledWithdrawals[msg.sender][_withdrawCounter] = shares;
+
+        emit ScheduleWithdraw(msg.sender, _withdrawCounter, shares);
     }
 
     /**
@@ -317,6 +319,8 @@ contract RibbonThetaVault is DSMath, OptionsVaultStorage {
         scheduledWithdrawals[msg.sender][withdrawID] = 0;
         queuedWithdrawShares = queuedWithdrawShares.sub(withdrawShares);
         uint256 withdrawAmount = _withdraw(withdrawShares);
+
+        emit ScheduledWithdrawCompleted(msg.sender, withdrawID, withdrawAmount);
 
         if (asset == WETH) {
             IWETH(WETH).withdraw(withdrawAmount);

d6d12ce202e98fafbe6fc86a828f3b17b13c4a7c
kenchangh <guanhao3797@gmail.com>
2021-05-04 09:51:06 +0100

    Remove old src files

--- src/connection.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-
-export const getProvider = () => {
-  if (!process.env["MAINNET_URI"]) {
-    throw new Error("MAINNET_URI not set");
-  }
-  return new ethers.providers.JsonRpcProvider(process.env["MAINNET_URI"], 1);
-};

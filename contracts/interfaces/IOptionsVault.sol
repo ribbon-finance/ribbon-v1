ce61b230a9e7f89b19ed5a639453e0a0a9041625
kenchangh <guanhao3797@gmail.com>
2021-05-06 09:20:07 +0100

    Withdraw to vault

---
+++ contracts/interfaces/IOptionsVault.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.2;
+
+interface IOptionsVault {
+    function deposit(uint256 amount) external;
+
+    function withdraw(uint256 shares) external;
+}
